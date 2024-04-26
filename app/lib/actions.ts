1a069a6fb570aa458853c647d2a4a7ce05ef2b16
muadz <muadzmohamad13@gmail.com>
2024-04-26 10:52:13 +0700

    adding updating invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true})
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -31,4 +32,21 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id:string, formData:FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    })
+
+    const amountInCents = amount * 100;
+    await sql `
+        UPDATE invoices set customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        where id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
 }
